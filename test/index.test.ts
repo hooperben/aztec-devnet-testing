390eaa98e4a1b55afc14dba66151ff375724f878
Ben Hooper <ben@labrys.io>
2024-09-09 12:13:44 +1000

    chore: got test passing

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,44 @@
-describe("testing aztec contract", () => {
-  test("hello", async () => {
-    expect(3).toEqual(3);
+import {
+  AccountWallet,
+  CompleteAddress,
+  ContractDeployer,
+  createDebugLogger,
+  Fr,
+  PXE,
+  waitForPXE,
+  TxStatus,
+  createPXEClient,
+  getContractInstanceFromDeployParams,
+  DebugLogger,
+} from "@aztec/aztec.js";
+import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
+import { beforeAll, describe, expect, test } from "vitest";
+
+const setupSandbox = async () => {
+  const PXE_URL = "http://localhost:8080";
+  const pxe = createPXEClient(PXE_URL);
+  await waitForPXE(pxe);
+  return pxe;
+};
+
+describe("aztec contract testing", async () => {
+  let pxe: PXE;
+  let wallets: AccountWallet[] = [];
+  let accounts: CompleteAddress[] = [];
+  let logger: DebugLogger;
+
+  beforeAll(async () => {
+    logger = createDebugLogger("aztec:aztec-starter");
+    logger.info("Aztec-Starter tests running.");
+
+    pxe = await setupSandbox();
+
+    wallets = await getInitialTestAccountsWallets(pxe);
+    accounts = wallets.map((w) => w.getCompleteAddress());
+  });
+
+  test("Deploys the contract", async () => {
+    const salt = Fr.random();
+    console.log(salt);
   });
 });
